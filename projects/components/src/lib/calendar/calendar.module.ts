149427a5caeea636fc7d5dd1c64bf91093ef0603
Edgar Alonso <ealonso@okode.com>
2019-05-06 13:15:31 +0200

    Removed custom palette dependency at calendar component

--- projects/components/src/lib/calendar/calendar.module.ts
+++ projects/components/src/lib/calendar/calendar.module.ts
@@ -5,14 +5,12 @@ import { IonicModule } from '@ionic/angular';
 import { CalendarComponent } from './calendar.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { OkodeNgxCustomPaletteModule } from 'custom-palette';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    OkodeNgxCustomPaletteModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
