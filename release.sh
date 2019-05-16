cef8b37eb303899f95795c6b75d776df014e96cc
Pedro Jorquera <pjorquera@okode.com>
2019-05-16 21:08:16 +0200

    Refactor build script

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail
+
+if [[ $# -ne 2 ]]; then
+    echo "Syntax: release [CURRENT_VERSION] [NEXT_VERSION]"
+    exit 1
+fi
+
+if [[ -n $(git status -z) ]]; then
+    echo "Repository not clean, ensure you have committed all your changes"
+    exit 1
+fi
+
+CURRENT=$1
+NEXT=$2
+
+# Create new tag
+git tag -a $CURRENT -m $CURRENT
+
+# Publish tag
+git push --tags
+
+# Bump version
+sed -i '' "s/$CURRENT/$NEXT/" projects/common/package.json
+sed -i '' "s/$CURRENT/$NEXT/" projects/components/package.json
+sed -i '' "s/$CURRENT/$NEXT/" projects/custom-palette/package.json
+
+# Update develop with new bumped version
+git commit -a -m"Bumped version ($NEXT) [ci skip]"
+git push
