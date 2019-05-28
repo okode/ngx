e2a11e229d8836a87b52414fb91dc5a7e9e498bb
Pedro Jorquera <pjorquera@okode.com>
2019-05-28 22:01:31 +0200

    Added support for MMobile config file

--- projects/common/src/lib/environment.service.ts
+++ projects/common/src/lib/environment.service.ts
@@ -9,7 +9,8 @@ export class Environment {
   private static isReady = false;
   private static environmentConfig: any = {};
 
-  private readonly JSON_PATH = 'assets/environments.json';
+  private readonly ENVIRONMENTS_JSON_PATH = 'assets/environments.json';
+  private readonly MMOBILE_JSON_PATH = 'assets/config/config.json';
   private readonly SELECTED_ENVIRONMENT_KEY = 'selectedEnvironmentKey';
   private isInitializing = false;
   private readyPromiseResolve: ((value?: void | PromiseLike<void>) => void)[] = [];
@@ -41,39 +42,49 @@ export class Environment {
     });
   }
 
-  private initEnvs() {
-    this.http.get(this.JSON_PATH).subscribe(json => {
-      if (json == null || Object.keys(json).length === 0) {
+  private async initEnvs() {
+
+    let json: Object;
+
+    try {
+      json = await this.http.get(this.ENVIRONMENTS_JSON_PATH).toPromise();
+    } catch (error) {
+      try {
+        json = await this.http.get(this.MMOBILE_JSON_PATH).toPromise();
+      } catch (error) {
+        console.log(error);
+        console.error(`EnvironmentService fails: Not found in config file`);
         this.readyPromiseReject.forEach((reject) => reject());
-        console.error(`EnvironmentService fails: '${this.JSON_PATH}' is empty or invalid`);
-        return;
       }
-      const environments = Object.keys(json).filter(environment => environment !== 'default');
-      if (environments == null || environments.length === 0) {
-        this.setEnv(false, 'default', json['default']);
-      } else {
-        this.storage.ready().then(() => {
-          this.storage.get(this.SELECTED_ENVIRONMENT_KEY).then(storedEnvironment => {
-            if (storedEnvironment == null) {
-              if (environments.length > 1) {
-                console.log('No saved environment detected, will prompt user for selection');
-                this.showEnvironmentActionSheet(environments, json);
-              } else {
-                const environment = environments[0];
-                this.setEnv(true, environment, json['default'], json[environment]);
-              }
-            } else {
-              console.log(`Detected saved environment: ${storedEnvironment}`);
-              this.setEnv(false, storedEnvironment, json['default'], json[storedEnvironment]);
-            }
-          });
-        });
-      }
-    }, err => {
-      console.log(err);
-      console.error(`EnvironmentService fails: Not found '${this.JSON_PATH}'`);
+    }
+
+    if (json == null || Object.keys(json).length === 0) {
       this.readyPromiseReject.forEach((reject) => reject());
-    });
+      console.error(`EnvironmentService fails: Config file is empty or invalid`);
+      return;
+    }
+
+    const environments = Object.keys(json).filter(environment => environment !== 'default');
+    if (environments == null || environments.length === 0) {
+      this.setEnv(false, 'default', json['default']);
+    } else {
+      this.storage.ready().then(() => {
+        this.storage.get(this.SELECTED_ENVIRONMENT_KEY).then(storedEnvironment => {
+          if (storedEnvironment == null) {
+            if (environments.length > 1) {
+              console.log('No saved environment detected, will prompt user for selection');
+              this.showEnvironmentActionSheet(environments, json);
+            } else {
+              const environment = environments[0];
+              this.setEnv(true, environment, json['default'], json[environment]);
+            }
+          } else {
+            console.log(`Detected saved environment: ${storedEnvironment}`);
+            this.setEnv(false, storedEnvironment, json['default'], json[storedEnvironment]);
+          }
+        });
+      });
+    }
   }
 
   private setEnv(save: boolean, environmentKey: string, configBase: any, configEnvironment?: any) {
