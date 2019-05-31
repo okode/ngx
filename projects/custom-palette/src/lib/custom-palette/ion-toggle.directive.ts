78f19b68bcc9849a999abc792095759a87b854d9
Álex Díaz <adiaz@okode.com>
2019-05-31 15:54:16 +0200

    Fixed ion-toogle lables (custom-palette)

--- projects/custom-palette/src/lib/custom-palette/ion-toggle.directive.ts
+++ projects/custom-palette/src/lib/custom-palette/ion-toggle.directive.ts
@@ -8,57 +8,58 @@ export class IonToggleDirective implements OnInit, OnChanges {
   @Input() enabledText = '';
   @Input() disabledText = '';
 
-  private shadowCss = `
-    :host {
-      width: 80px;
-    }
-    :host(.toggle-checked) .toggle-inner {
-      -webkit-transform: translate3d(48px, 0, 0);
-      transform: translate3d(48px, 0, 0);
-    }
-    :host .toggle-icon:after {
-      line-height: 33px;
-      color: white;
-      text-transform: uppercase;
-      font-weight: 800;
-      font-size: 13px;
-      width: 50px;
-      display: inline-block;
-      text-align: center;
-    }
-    :host(.toggle-checked) .toggle-icon:after {}
-    :host(:not(.toggle-checked)) .toggle-icon:after {
-      position: absolute;
-      margin-left: 28px;
-    }
-    :host(:not(.toggle-checked)) .toggle-icon {
-      background-color: #919191;
-    }
-    :host(:not(.toggle-checked)) .toggle-icon:before {
-      background: #919191;
-    }
-  `;
-
   constructor(private el: ElementRef) {}
 
   ngOnInit() {
-    this.setShadowStyle(this.shadowCss);
+    this.setShadowStyle();
   }
 
   ngOnChanges(changes) {
     if (changes && (changes.enabledText || changes.disabledText)) {
-      this.setShadowStyle(`<style>
-        :host(.toggle-checked) .toggle-icon:after { content: '${this.enabledText}'; }
-        :host(:not(.toggle-checked)) .toggle-icon:after { content: '${this.disabledText}'; }
-      </style>`);
+      this.setShadowStyle();
     }
   }
 
-  private setShadowStyle(style) {
+  private setShadowStyle() {
     try {
       const shadow = this.el.nativeElement.shadowRoot ||
                      this.el.nativeElement.attachShadow({ mode: 'open' });
-      if (shadow) { shadow.innerHTML += `<style>${style}</style>`; }
+      if (shadow) { shadow.innerHTML += `
+        <style>
+          :host {
+            width: 80px;
+          }
+          :host(.toggle-checked) .toggle-inner {
+            -webkit-transform: translate3d(48px, 0, 0);
+            transform: translate3d(48px, 0, 0);
+          }
+          :host .toggle-icon:after {
+            content: ' ';
+            line-height: 33px;
+            color: white;
+            text-transform: uppercase;
+            font-weight: 800;
+            font-size: 13px;
+            width: 50px;
+            display: inline-block;
+            text-align: center;
+          }
+          :host(.toggle-checked) .toggle-icon:after {
+            content: '${this.enabledText}';
+          }
+          :host(:not(.toggle-checked)) .toggle-icon:after {
+            content: '${this.disabledText}';
+            position: absolute;
+            margin-left: 28px;
+          }
+          :host(:not(.toggle-checked)) .toggle-icon {
+            background-color: #919191;
+          }
+          :host(:not(.toggle-checked)) .toggle-icon:before {
+            background: #919191;
+          }
+        </style>
+      `; }
     } catch (e) {}
   }
 
